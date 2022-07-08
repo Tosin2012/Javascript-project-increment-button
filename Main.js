d79bbc07ff53159ed88e37a7a6bdd57a0107c2af
Tosin2012 <adedipetosin2012@gmail.com>
2022-07-08 23:26:58 +0100

    my first commit

---
+++ Main.js
@@ -0,0 +1,15 @@
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+console.log(count)
+
+/*function increment() {
+    console.log("teh button was clicked")
+}*/
